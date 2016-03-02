a16d01e65c7226652af46c025b3901ea315f2547
Your Name <your@example.com>
2016-03-02 10:13:18 -0500

    a.

---
+++ Git Practice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 9;
+            int c = a + b;
+            Console.WriteLine(c);
+        }
+    }
+}
